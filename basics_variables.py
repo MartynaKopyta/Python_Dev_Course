33a5b09563cc360ce6e05809c044646690372721
MartynaKopyta <martyna.kopyta@gmail.com>
2023-08-28 18:27:17 +0200

    variables notes

---
+++ basics_variables.py
@@ -0,0 +1,22 @@
+#General rules in python
+#snake_case -> all lowercase + underscores
+#start with lowercase or underscore
+#case-sensitive -> num_1 != nuM_1
+#can't overwrite keywords
+
+user_iq = 190
+print(user_iq)
+
+user_sth = user_iq/4
+print(user_sth)
+
+#Constants convention -> capital letters
+PI = 3.14
+
+#Dunder starts with 2 underscores
+#you can start variable with __ but convention is you shouldn't
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
