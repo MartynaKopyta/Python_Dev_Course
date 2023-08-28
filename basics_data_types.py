d0bf276cda6d1914d9c648ea68c1577ebfcdfb3d
MartynaKopyta <martyna.kopyta@gmail.com>
2023-08-28 17:46:42 +0200

    added data types notes

---
+++ basics_data_types.py
@@ -0,0 +1,38 @@
+#Ctrl / - multiple line comment
+
+name = input("What is your name? ")
+print("Hello " + name + " !")
+print("Hello", name, "!")
+
+#Data Types:
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+#check type
+print(type(2 + 4))
+print(type(2 / 4))
+
+#Classes -> custom types
+
+#Specialized Data Types -> packages, modules
+
+#None = Null
+
+#Operators apart from the obvious:
+#2 to the power of 3
+print(2 ** 3)
+#5 divided to 4 and rounded to an integer
+print(5 // 4)
+#remainder of this devision
+print(5 % 4)
+
+#Math functions
+print(round(3.1))
+print(abs(-20)) #absolute value
+
