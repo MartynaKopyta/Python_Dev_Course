89f50332d8017e2f26163cadcf9b8479e01fe14e
MartynaKopyta <martyna.kopyta@gmail.com>
2023-09-11 16:51:59 +0200

    reorganised files, added exercise file

--- exercise_type_conversion.py
+++ exercise_type_conversion.py
@@ -2,3 +2,5 @@
 birth_year = input('What year were you born? ')
 age = 2023 - int(birth_year)
 print('You are ' + str(age) + ' years old!')
+
+# age = 2019 - bool(birth_year) would result in 2018, cause True is 1
