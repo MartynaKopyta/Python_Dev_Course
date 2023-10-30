2c8f9b464b50a4262ac3c62730b8afed08d65aa5
MartynaKopyta <martyna.kopyta@gmail.com>
2023-10-30 13:24:02 +0100

    added section note

---
+++ ML_DS/ML_101.py
@@ -0,0 +1,8 @@
+# 1 - import the data
+# 2 - clean the data
+# 3 - split the data - training set, test set
+# 4 - create a model
+# 5 - check the output
+# 5 - improve
+
+# code in jupyter notebooks ML_DS Folder
