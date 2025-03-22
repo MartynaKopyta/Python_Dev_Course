9ad9e35d9462408c688feecb6c66e5ef88e557b5
MartynaKopyta <martyna.kopyta@gmail.com>
2025-03-22 13:31:08 +0100

    added more modules lessons

--- modules/main.py
+++ modules/main.py
@@ -1,10 +1,30 @@
 import utility
 from shopping import shopping_cart
+# import random
+# or we can be more explicit with the import statement  
+from random import shuffle
 # or import shopping.shopping_cart
 
 def main():
     print(utility.multiply(10, 2))
-    print(shopping_cart.buy("apple"))
+    cart = shopping_cart.buy("apple")
+    print(cart)
+    cart = shopping_cart.buy_anything(cart)
+    shuffle(cart) # Example of random module use - shuffle a list
+    print(cart)
 
 if __name__ == "__main__":
     main()
+
+'''
+Other useful built-in modules:
+- sys - ex. sys.argv it allows as to pass arguments to the script from the command line. 
+For example we can have a python file script.py with content  like this:
+import sys
+first = sys.argv[1] # we start from one, cause the first argument is the name of the script
+second = sys.argv[2]
+print(f'Hi {first} {second}')
+
+And we can run it from the command line like this:
+python script.py John Doe
+'''
--- modules/shopping/shopping_cart.py
+++ modules/shopping/shopping_cart.py
@@ -1,4 +1,15 @@
-def buy(item):
-    cart = []
+import random
+
+def buy(item, cart=None):
+    if cart is None:
+        cart = []
     cart.append(item)
+    return cart
+
+def buy_anything(cart=None):
+    # Example of random module use - random choice from a list
+    random_items = ["banana", "cherry", "date", "elderberry"]
+    if cart is None:
+        cart = []
+    cart.append(random.choice(random_items))
     return cart
